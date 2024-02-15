41d19e0c467c3044bdaaafe9fedfb49a2664187f
gursi26 <gursimar.batra@gmail.com>
2024-02-15 14:17:53 -0500

    finished initial arg parsing

---
+++ src/args.rs
@@ -0,0 +1,67 @@
+use clap::{Args, Parser, Subcommand, ArgAction};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct MusicPlayerArgs {
+    #[clap(subcommand)]
+    pub action_type: ActionType
+}
+
+#[derive(Debug, Subcommand)]
+pub enum ActionType {
+    /// Adds a song to a specified playlist (or to "liked" if no playlist specified)
+    Add(AddArgs), 
+
+    /// Removed a song from a specified playlist (or "liked" if no playlist specified)
+    Remove(RemoveArgs),
+
+    /// Creates a new playlist
+    Create(CreateArgs),
+
+    /// Plays a specific playlist (or "liked" if no playlist specified)
+    Play(PlayArgs)
+}
+
+#[derive(Debug, Args)]
+pub struct AddArgs {
+    /// The search term for the song to be added
+    #[arg(short = 'q', long = "query-term")]
+    pub query_term: String,
+
+    /// The playlist to add the song to
+    #[arg(short = 'p', long = "playlist", default_value = "liked")]
+    pub playlist: String,
+
+    /// The number of search results to display
+    #[arg(short = 'c', long = "count")]
+    pub count: i8
+}
+
+#[derive(Debug, Args)]
+pub struct RemoveArgs {
+    /// The search term for the song to be removed (Optional, lists all songs in playlist if
+    /// unspecified)
+    #[arg(short = 'q', long = "query-term", default_value = None)]
+    pub query_term: Option<String>,
+
+    /// The playlist from which the song is to be removed
+    #[arg(short = 'p', long = "playlist", default_value = "liked")]
+    pub playlist: String,
+}
+
+#[derive(Debug, Args)]
+pub struct CreateArgs {
+    /// Name of new playlist to be created
+    #[arg(short = 'n', long = "name")]
+    pub playlist_name: String,
+}
+
+#[derive(Debug, Args)]
+pub struct PlayArgs {
+    /// The playlist to be played
+    #[arg(short = 'p', long = "playlist", default_value = "liked")]
+    pub playlist: String,
+
+    #[arg(short = 's', long = "shuffle", action = ArgAction::SetTrue)]
+    pub shuffle: bool
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
-use clap::{Args, Parser, Subcommand};
-
-#[derive(Debug, Parser)]
-#[clap(author, version, about)]
-pub struct MusicPlayerArgs {
-    #[clap(subcommand)]
-    pub action_type: ActionType
-}
-
-#[derive(Debug, Subcommand)]
-pub enum ActionType {
-    /// Adds a song to a specified playlist (or to "liked" if no playlist specified)
-    Add(AddArgs), 
-}
-
-#[derive(Debug, Args)]
-pub struct AddArgs {
-    /// The search term for the song to be added
-    #[arg(short = 'q', long = "query-term")]
-    pub query_term: String,
-
-    /// The playlist to add the song to
-    #[arg(short = 'p', long = "playlist", default_value = "liked")]
-    pub playlist: String,
-
-    /// The number of search results to display
-    #[arg(short = 'c', long = "count")]
-    pub count: i8
-}
+mod args;
+use clap::Parser;
+use args::*;
 
 fn main() {
     let args = MusicPlayerArgs::parse();
-    let addArgs = match args.action_type {
-        ActionType::Add(ref add_args) => add_args
+    match args.action_type {
+        ActionType::Add(ref add_args) => mprs_add(add_args),
+        ActionType::Remove(ref remove_args) => mprs_remove(remove_args),
+        ActionType::Create(ref create_args) => mprs_create(create_args),
+        ActionType::Play(ref play_args) => mprs_play(play_args),
     };
-    println!("{:?}", addArgs.query_term);
+}
+
+fn mprs_add(args: &AddArgs) {
+    println!("{:?}", args);
+}
+
+fn mprs_remove(args: &RemoveArgs) {
+    println!("{:?}", args);
+}
+
+fn mprs_create(args: &CreateArgs) {
+    println!("{:?}", args);
+}
+
+fn mprs_play(args: &PlayArgs) {
+    println!("{:?}", args);
 }
