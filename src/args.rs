7f7c042f58fc2e75c553a1758aebe57aab868db3
gursi26 <gursimar.batra@gmail.com>
2024-02-17 14:45:25 -0500

    project rename to mprs

--- src/args.rs
+++ src/args.rs
@@ -33,7 +33,7 @@ pub struct AddArgs {
     pub playlist: String,
 
     /// The number of search results to display
-    #[arg(short = 'c', long = "count")]
+    #[arg(short = 'c', long = "count", default_value_t = 10)]
     pub count: i8
 }
 
