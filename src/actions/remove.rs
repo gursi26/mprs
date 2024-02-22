2891948725a36ce06177f369d9e8e0c5e87f4b96
quiz3 <113859565+quiz3@users.noreply.github.com>
2024-02-22 02:01:54 -0500

    working on remove action

--- src/actions/remove.rs
+++ src/actions/remove.rs
@@ -1,7 +1,39 @@
 use crate::args::RemoveArgs;
 use crate::config::UserConfig;
+use std::io::{stdout, Write};
+use mprs::utils::print_table;
+use std::fs::remove_file;
+use std::fs::read_dir;
+// these are for read_dir
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
 
 pub fn mprs_remove(args: &RemoveArgs, config: &UserConfig) {
+    println!("\nDEBUG INFO:");
     println!("{:?}", config);
     println!("{:?}", args);
+
+    // Unwrap args.query_term
+    let query = args.query_term.as_deref().unwrap_or("None");
+
+    println!(
+        "Query: {}\nPlaylist: {}",
+        query, args.playlist
+    );
+
+    let mut playlist_path = config.base_dir.clone();
+    playlist_path.push(&args.playlist);
+    println!("Playlist path: {:?}", playlist_path);
+
+    // IO copied from add.rs
+    print!("Select song by number : ");
+    let _ = stdout().flush();
+
+    let mut input_string = String::new();
+    std::io::stdin().read_line(&mut input_string).unwrap();
+    let id_idx: i32 = input_string.trim().parse().unwrap();
+
+    // TODO: figure out how to print the number of songs in a playlist if no query term is given
+
 }
