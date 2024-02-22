512727af3cf5d4ac69bafb608cf99bb6e3b8c14d
gursi26 <gursimar.batra@gmail.com>
2024-02-22 13:41:55 -0500

    replaced afplay with rodio, added pause, play, volume and speed change options

--- src/actions/play.rs
+++ src/actions/play.rs
@@ -11,6 +11,11 @@ use std::time;
 use rand::thread_rng;
 use rand::seq::SliceRandom;
 use std::time::Instant;
+use std::fs::File;
+use std::io::BufReader;
+use std::time::Duration;
+use rodio::{Decoder, OutputStream, Sink};
+use rodio::source::{SineWave, Source};
 
 use termion;
 use termion::input::TermRead;
@@ -104,52 +109,78 @@ pub fn mprs_play(args: &PlayArgs, config: &UserConfig) {
 
     let mut stdout = io::stdout().into_raw_mode().unwrap();
     let mut stdin = termion::async_stdin().keys();
-    let mut quit_flag;
+    let mut quit_flag: bool = false;
     let mut i: i32 = 0;
+    let mut curr_volume = 1.0;
+    let mut curr_speed = 1.0;
 
-    while (i as usize) < song_queue.len() {
-        let curr_song = song_queue[i as usize].clone();
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+
+    while (i as usize) < song_queue.len() && !quit_flag {
+        // Get and print current song
+        let curr_song = &song_queue[i as usize];
         println!("Current song: {}", curr_song.as_path().file_name().unwrap().to_str().unwrap());
-        let mut cmd = Command::new("afplay")
-            .arg(curr_song)
-            .kill_on_drop(true)
-            .spawn()
-            .unwrap();
-        let time_since_start = Instant::now();
 
-        quit_flag = false;
+        // append current song to sink
+        sink.append(Decoder::new(BufReader::new(File::open(curr_song).unwrap())).unwrap());
+        sink.set_volume(curr_volume);
+        sink.set_speed(curr_speed);
+        let start_time = Instant::now();
 
-        while cmd.try_status().unwrap() == None {
+        while !sink.empty() {
             let input = stdin.next();
             if let Some(Ok(key)) = input {
                 match key {
                     termion::event::Key::Char('q') => {
-                        drop(cmd);
+                        sink.stop();
                         quit_flag = true;
                         break;
                     }
                     termion::event::Key::Char('n') => {
-                        drop(cmd);
+                        sink.stop();
                         break;
                     }
                     termion::event::Key::Char('b') => {
-                        drop(cmd);
-                        i = i - 1;
-                        if time_since_start.elapsed().as_secs() < TIME_UNTIL_BACK_SELF {
-                            i = i - 1
+                        sink.stop();
+                        i -= 1;
+                        if start_time.elapsed().as_secs() < TIME_UNTIL_BACK_SELF {
+                            i -= 1;
                         }
                         break;
+                    }
+                    termion::event::Key::Char('p') => {
+                        if sink.is_paused() {
+                            sink.play();
+                        } else {
+                            sink.pause();
+                        }
+                    }
+                    termion::event::Key::Char('+') => {
+                        curr_volume += 0.1;
+                        sink.set_volume(curr_volume);
+                    }
+                    termion::event::Key::Char('-') => {
+                        curr_volume -= 0.1;
+                        sink.set_volume(curr_volume);
+                    }
+                    termion::event::Key::Right => {
+                        curr_speed += 0.1;
+                        sink.set_speed(curr_speed);
+                    }
+                    termion::event::Key::Left => {
+                        curr_speed -= 0.1;
+                        sink.set_speed(curr_speed);
+                    }
+                    termion::event::Key::Up => {
+                        curr_speed = 1.0;
+                        sink.set_speed(curr_speed);
                     }
                     _ => {}
                 }
             }
             thread::sleep(time::Duration::from_millis(50));
         }
-
         i += 1;
-
-        if quit_flag {
-            break;
-        }
     }
 }
