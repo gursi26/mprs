2a8693e89b11be6be5390e3f1eff27e48bcb1b41
gursi26 <gursimar.batra@gmail.com>
2024-03-02 22:01:15 -0500

    added frame interpolation

--- visualizer-trial/src/main.rs
+++ visualizer-trial/src/main.rs
@@ -10,58 +10,76 @@ use std::fs::File;
 use std::io::{stdout, BufReader, Write};
 use std::usize;
 // use macroquad::prelude::*;
-use std::{thread, time};
 use raylib::prelude::*;
+use std::{thread, time};
 
-const FREQUENCY_RESOLUTION: u32 = 50;
+const FREQUENCY_RESOLUTION: u32 = 100;
 const FREQ_WINDOW_LOW: f32 = 0.0;
-const FREQ_WINDOW_HIGH: f32 = 20000.0;
+const FREQ_WINDOW_HIGH: f32 = 5000.0;
 const MONO_SCALING_FACTOR: i32 = 10;
-const FFT_FPS: u32 = 30;
+const FFT_FPS: u32 = 15;
+const RENDERING_FPS: u32 = FFT_FPS * 5;
 const FFT_WINDOW: i32 =
     ((256 as u64 / 107 as u64) * FREQUENCY_RESOLUTION as u64).next_power_of_two() as i32;
 
-
 // const AUDIO_PATH: &str = "/Users/gursi/Desktop/60 BPM - Metronome-[AudioTrimmer.com].mp3";
-const AUDIO_PATH: &str = "/Users/gursi/Desktop/gurenge.mp3";
-
-
-
+// const AUDIO_PATH: &str = "/Users/gursi/Desktop/gurenge.mp3";
+const AUDIO_PATH: &str = "/Users/gursi/mprs-music/rn/ハルジオン.mp3";
 
 fn main() {
-    let fft = get_fft();
+    let mut fft = get_fft().into_iter().peekable();
     let mut i = 0;
 
     let (mut rl, thread) = raylib::init()
         .size(640, 480)
         .title("audio-visualizer-trial")
         .build();
-    rl.set_target_fps(FFT_FPS);
+    rl.set_target_fps(RENDERING_FPS);
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let file = BufReader::new(File::open(AUDIO_PATH).unwrap());
     let source = Decoder::new(file).unwrap();
     stream_handle.play_raw(source.convert_samples());
 
-    while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+    let num_frame_gen = RENDERING_FPS / FFT_FPS;
+    let mut fft_chunk: Vec<f32> = Vec::new();
 
+    while !rl.window_should_close() && fft.peek().is_some() {
+        let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
-        if i >= fft.len() {
-            break;
+
+        if i as u32 % num_frame_gen == 0 {
+            fft_chunk = fft.next().unwrap();
+        } else {
+            let next_chunk = fft.peek().unwrap();
+            fft_chunk = interpolate_vecs(&fft_chunk, next_chunk);
         }
 
-        let fft_chunk = &fft[i as usize];
         let (h, w) = (d.get_screen_height(), d.get_screen_width());
-        let bar_start_idxs = (0..((w as i32) + 1)).step_by(w as usize / FREQUENCY_RESOLUTION as usize).collect::<Vec<i32>>();
+        let bar_start_idxs = (0..((w as i32) + 1))
+            .step_by(w as usize / fft_chunk.len() as usize)
+            .collect::<Vec<i32>>();
 
-        for j in 0..(FREQUENCY_RESOLUTION as usize) {
+        for j in 0..(fft_chunk.len() as usize) {
             let curr_fft_value = (fft_chunk[j] * h as f32 * 2.0) as i32;
             let (start_x_id, end_x_id) = (bar_start_idxs[j], bar_start_idxs[j + 1]);
-            d.draw_rectangle(start_x_id, h - curr_fft_value, end_x_id - start_x_id, curr_fft_value, Color::GREEN);
+            d.draw_rectangle(
+                start_x_id,
+                h - curr_fft_value,
+                end_x_id - start_x_id,
+                curr_fft_value,
+                Color::GREEN,
+            );
         }
-         i += 1;
+        i += 1;
     }
+}
+
+fn interpolate_vecs(v1: &Vec<f32>, v2: &Vec<f32>) -> Vec<f32> {
+    v1.iter()
+        .zip(v2.iter())
+        .map(|(x, y)| (x.clone() + y.clone()) / 2.0)
+        .collect::<Vec<f32>>()
 }
 
 fn get_fft() -> Vec<Vec<f32>> {
@@ -82,6 +100,7 @@ fn get_fft() -> Vec<Vec<f32>> {
     let mut output_vec = Vec::new();
 
     let (mut min, mut max): (f32, f32) = (100.0, 0.0);
+    println!("Computing FFT...");
 
     for x in sample_vec.chunks((sample_rate * n_channels) as usize / FFT_FPS as usize) {
         let frame = &x[..(FFT_WINDOW as usize * n_channels as usize)];
@@ -112,7 +131,7 @@ fn get_fft() -> Vec<Vec<f32>> {
             max = max.max(*val);
             min = min.min(*val);
         }
-        output_vec.push(curr_vec);
+        output_vec.push(curr_vec.clone());
     }
 
     let scale = 1.0 / (max - min);
@@ -122,7 +141,6 @@ fn get_fft() -> Vec<Vec<f32>> {
             output_vec[j][k] *= scale;
         }
     }
-
 
     println!("Min: {}, Max: {}", min, max);
     println!("{}, {}", output_vec.len(), output_vec[0].len());
